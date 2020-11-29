24061a4f9f2c7eaef7be8e525813ba1bfba6befc
Iago <iagobrazmendes@gmail.com>
2020-11-29 12:40:25 -0300

    create global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,36 @@
+import {createGlobalStyle} from "styled-components";
+
+export default createGlobalStyle`
+	:root
+	{
+		font-size: 10px;
+	}
+
+  *
+	{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body
+  {
+    background-color: ${props => props.theme.colors.background};
+    color: ${props => props.theme.colors.text};
+  }
+
+  #__next
+  {
+    display: flex;
+		width: 100vw;
+		height: 100vh;
+  }
+
+	.page
+	{
+		height: 100vh;
+    width: 100%;
+    overflow-y: auto;
+		overflow-x: hidden;
+	}
+`
