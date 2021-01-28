dea7c085754deba885f0d89196bc1f22866642d9
Iago <iagobrazmendes@gmail.com>
2021-01-28 17:19:20 -0300

    improve delete

--- src/components/Delete.tsx
+++ src/components/Delete.tsx
@@ -1,8 +1,14 @@
 import {useRouter} from 'next/router'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 import Container from '../styles/components/Delete'
 import {FiTrash} from 'react-icons/fi'
 import api from '../services/api'
+import successAlert from '../utils/alerts/success'
+import errorAlert from '../utils/alerts/error'
+
+const MySwal = withReactContent(Swal)
 
 interface DeleteProps
 {
@@ -12,23 +18,37 @@ interface DeleteProps
 
 const Delete: React.FC<DeleteProps> = ({collection, id}) =>
 {
-	const Router = useRouter()
+	const {back} = useRouter()
 
-	async function handleClick()
+	function handleClick()
 	{
-		const yes = confirm('Are you sure you want to delete this item?')
-		if (yes)
-			await api.delete(`${collection}/${id}`)
-				.then(() =>
-				{
-					alert('Item deleted successfully!')
-					Router.back()
-				})
-				.catch(err =>
-				{
-					alert('Some error occurred!')
-					console.error(err)
-				})
+		MySwal.fire(
+			{
+				icon: 'question',
+				title: 'Are you sure?',
+				text: `If you continue, this item will be deleted!`,
+				showCancelButton: true,
+				confirmButtonText: 'Continue'
+			})
+			.then(res =>
+			{
+				if (res.isConfirmed)
+					handleRemove()
+			})
+	}
+
+	function handleRemove()
+	{
+		api.delete(`${collection}/${id}`)
+			.then(() =>
+			{
+				successAlert('Item successfully deleted!')
+				back()
+			})
+			.catch(err =>
+			{
+				errorAlert(err.response.data.message)
+			})
 	}
 
 	return (
